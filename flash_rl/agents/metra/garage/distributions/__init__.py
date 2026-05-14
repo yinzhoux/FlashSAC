579dc58023393550b530746eb34554af4135c69d
btx0424 <btx0424@outlook.com>
2026-05-14 15:58:51 +0800

    update: intergrate garage

---
+++ flash_rl/agents/metra/garage/distributions/__init__.py
@@ -0,0 +1,4 @@
+"""PyTorch Custom Distributions."""
+from .tanh_normal import TanhNormal
+
+__all__ = ['TanhNormal']
---
+++ flash_rl/agents/metra/garage/modules/__init__.py
@@ -0,0 +1,15 @@
+"""Pytorch modules."""
+
+from .gaussian_mlp_module import \
+    GaussianMLPIndependentStdModule, GaussianMLPModule, \
+    GaussianMLPTwoHeadedModule
+from .mlp_module import MLPModule
+from .multi_headed_mlp_module import MultiHeadedMLPModule
+
+__all__ = [
+    'MLPModule',
+    'MultiHeadedMLPModule',
+    'GaussianMLPModule',
+    'GaussianMLPIndependentStdModule',
+    'GaussianMLPTwoHeadedModule',
+]
