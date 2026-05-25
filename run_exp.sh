f5c1129370f2a87b99c4dc9f25e95d00d49eeb23
yinzhoux <yinzhoux23@gmail.com>
2026-05-25 10:01:20 +0800

    update: align to metra train

--- run_exp.sh
+++ run_exp.sh
@@ -18,11 +18,13 @@ run_in_tmux() {
     echo "成功在会话 [$SESSION_NAME] 中启动任务，日志查看: tail -f $LOG_FILE"
 }
 
+root_dir=/home/helix/projects/METRA-Projects/FlashSAC
+
 # 1. sac normal training with:
 #    Reward norm; Encoder weight norm;
 # run_in_tmux "1-sac_normal" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-sac" \
 #                 --overrides exp_name=sac
@@ -33,7 +35,7 @@ run_in_tmux() {
 #    Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "2-metra_normal" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra \
 #                 --overrides exp_name=metra-normal"
@@ -46,7 +48,7 @@ run_in_tmux() {
 #    NO Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "4-metra-no-rewd-norm" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-no-rewd-norm \
 #                 --overrides exp_name=metra-normal-no-rewd-norm"
@@ -56,7 +58,7 @@ run_in_tmux() {
 #    NO Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "5-gaussian" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-gaussian \
 #                 --overrides exp_name=metra-gaussian"
@@ -65,7 +67,7 @@ run_in_tmux() {
 #    NO Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "6-simple" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-simple \
 #                 --overrides exp_name=metra-simple"
@@ -74,7 +76,7 @@ run_in_tmux() {
 #    Reward norm; NO Encoder weight norm; episode 1000, longer train
 # run_in_tmux "7-metra_normal-long" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base_long \
 #                 --overrides agent=metra \
 #                 --overrides exp_name=metra-normal-long"
@@ -83,7 +85,7 @@ run_in_tmux() {
 #    NO Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "8-discrete" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-discrete \
 #                 --overrides exp_name=metra-discrete"
@@ -94,7 +96,7 @@ run_in_tmux() {
 #    NO Reward norm; weight norm; episode 1000
 # run_in_tmux "9-encoder-weight-norm" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-encoder-weight-norm \
 #                 --overrides exp_name=metra-encoder-weight-norm"
@@ -103,7 +105,7 @@ run_in_tmux() {
 #    NO Reward norm; NO Encoder weight norm; episode 1000
 # run_in_tmux "10-discrete-larger" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-discrete-larger \
 #                 --overrides exp_name=metra-discrete-larger"
@@ -113,7 +115,7 @@ run_in_tmux() {
 #    Reward norm; Encoder weight norm; episode 1000
 # run_in_tmux "14-metra-low-lambda-init" \
 # "export MUJOCO_GL=egl && \
-# uv run python ~/zjx/FlashSAC/train.py \
+# uv run python ${root_dir}/train.py \
 #                 --config_name metra_base \
 #                 --overrides agent=metra-low-lambda-init \
 #                 --overrides exp_name=metra-normal-low-lambda"
@@ -123,7 +125,7 @@ run_in_tmux() {
 #    Reward norm; NO Encoder weight norm; episode 1000
 run_in_tmux "15-metra_normal-cpu" \
 "export MUJOCO_GL=egl && \
-uv run python ~/zjx/FlashSAC/train.py \
+uv run python ${root_dir}/train.py \
                 --config_name metra_base \
                 --overrides agent=metra-cpu \
                 --overrides exp_name=metra-normal_cpu" \
